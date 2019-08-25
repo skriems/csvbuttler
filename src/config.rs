3819c1b0a6ddfcf03f68ae9fbc58337f05c39206
Sebastian Kriems <bastoberto@gmx.de>
2019-08-25 23:21:14 +0200

    added basic auth creds to cli/config

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::path::Path;
 
-use crate::error::Error;
+use crate::error::{Error, ErrorKind};
 
 use dotenv;
 use structopt::StructOpt;
@@ -28,6 +28,14 @@ pub struct Config {
     /// Access-Control-Allow-Origin Header
     #[structopt(name = "allow-origin", short = "-o", long)]
     pub allow_origin: Option<String>,
+
+    /// Basic Auth username for fetching the CSV
+    #[structopt(name = "csv-username", short = "-u", long)]
+    pub csv_username: Option<String>,
+
+    /// Basic Auth password for fetching the CSV
+    #[structopt(name = "csv-password", short = "-w", long)]
+    pub csv_password: Option<String>,
 }
 
 impl Config {
@@ -60,6 +68,29 @@ pub fn get_config() -> Result<Config, Error> {
             Err(_) => None,
         };
         cfg.allow_origin = allowed_origin;
+    }
+
+    if cfg.csv_username.is_none() {
+        let csv_username = match env::var("CSV_USERNAME") {
+            Ok(username) => Some(username),
+            Err(_) => None,
+        };
+        cfg.csv_username = csv_username;
+    }
+
+    if cfg.csv_password.is_none() {
+        let csv_password = match env::var("CSV_PASSWORD") {
+            Ok(password) => Some(password),
+            Err(_) => None,
+        };
+        cfg.csv_password = csv_password;
+    }
+
+    // TODO can we move that validation to structopt somehow?
+    if cfg.csv_username.is_some() && cfg.csv_password.is_none() {
+        return Err(From::from(ErrorKind::Other(
+            "Need password for Basic Auth".into(),
+        )));
     }
     Ok(cfg)
 }
